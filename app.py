c5340fe5f9f7e8dcace4615ad9686d9aca375d31
ntaherian <taherian.negar@gmail.com>
2023-08-16 11:21:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,6 @@ def main():
             #fig = go.Figure()
             fig = plt.gcf()
             #x = pandas_ai.run(list(dataframes.values()), question)
-            x = pandas_ai.run(list(dataframes.values()), question)
             x = dataframes.chat(question)
             if fig.get_axes() > 0:
                 st.session_state.chat_history.append((st.session_state.input, fig))

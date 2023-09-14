0b8c7deb163ccab2a873a356aef032233f92380a
ntaherian <taherian.negar@gmail.com>
2023-09-14 11:54:33 -0400

    Update render.py

--- render.py
+++ render.py
@@ -3,8 +3,8 @@ import re
 
 
 bot_msg_container_html_template = '''
-<div style='background-color: #98D7C2; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
-    <div style="width: 20%; display: flex; justify-content: center">
+<div style='background-color: #2C3333; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
+    <div style="width: 20%; display: flex; justify-content: center ">
         <img src="https://w1.pngwing.com/pngs/278/853/png-transparent-line-art-nose-chatbot-internet-bot-artificial-intelligence-snout-head-smile-black-and-white-thumbnail.png" style="max-height: 50px; max-width: 50px; border-radius: 50%;">
     </div>
     <div style="width: 80%;">
@@ -14,13 +14,13 @@ bot_msg_container_html_template = '''
 '''
 
 user_msg_container_html_template = '''
-<div style='background-color: #98D7C2; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
+<div style='background-color: #2C3333; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
     <div style="width: 78%">
         $MSG
     </div>
     <div style="width: 20%; margin-left: auto; display: flex; justify-content: center;">
-        <img src="    https://storage.needpix.com/rsynced_images/user-2935527_1280.png" style="max-width: 50px; max-height: 50px; float: right; border-radius: 50%;">
-    </div>    
+        <img src="https://storage.needpix.com/rsynced_images/user-2935527_1280.png" style="max-width: 50px; max-height: 50px; float: right; border-radius: 50%;">
+    </div>
 </div>
 '''
 
